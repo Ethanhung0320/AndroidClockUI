c6bb3b10cd5544a2ae71e45c29f1b6f645a0fa05
Ethan <q40131125@gmail.com>
2024-04-08 11:04:10 +0800

    Initial Commit

---
+++ AndroidClockUI/AndroidClockUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndroidClockUIApp.swift
+//  AndroidClockUI
+//
+//  Created by Ethan Hung on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct AndroidClockUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndroidClockUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AndroidClockUI
+//
+//  Created by Ethan Hung on 2024/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
